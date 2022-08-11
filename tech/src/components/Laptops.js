4b01f73262aea91a3201cb15c9981d50db12a5a9
Vaibhavpatil99 <patilvb1999@gmail.com>
2022-08-11 16:05:11 +0530

    changes

--- tech/src/components/Laptops.js
+++ tech/src/components/Laptops.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 function Laptops() {
 
@@ -22,26 +23,26 @@ function Laptops() {
         console.log(updated)
         axios.post('http://localhost:3003/addtocart',updated).then(res=>console.log(res)).catch(err=>console.log(err))
 
-        alert("product Added to cart")
+        Swal.fire(
+            `${updated.Dname}`,
+            'Added to cart',
+            'success'
+          )
 
     }
 
 
     return (
-        <div className='bg-gray-300'>
-            <div>
-
-            </div>
-
+        <div className='bg-gray-100 py-4'>
             {/* {data?.map((e, index) => ( */}
-                {data?.filter(fil=>fil.category == "Laptop").map((e, index) => (
-                    <div className=" flex   mx-32">
-                    {console.log(e)}
+            {data?.filter(fil => fil.category === "Laptop").map((e, index) => (
+                <div className=" flex bg-white rounded-xl shadow-lg mx-32 my-4">
+                    {/* {console.log(e)} */}
 
-                    <div className="border w-3/12 flex justify-center items-center bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className="w-3/12 flex justify-center items-center  m-4 p-4">
                         <img className="h-48" src={e.img} alt="" />
                     </div>
-                    <div className="border w-6/12 bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className="w-6/12  m-4 p-4">
                         <h2 className="text-3xl font-semibold">{e.Dname}</h2>
                         <div>
                             {e.spec.map((specs) => (
@@ -51,7 +52,7 @@ function Laptops() {
                         <div className='text'>
                         </div>
                     </div>
-                    <div className=" flex flex-col justify-center items-center w-3/12 bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className=" flex flex-col justify-center items-center w-3/12 m-4 p-4">
                         <h2 className="text-3xl font-semibold">{e.price}₹</h2>
                         <div className='flex justify-around items-center text-white text-lg'>
                             <button className='bg-blue-400 p-2 rounded mx-1' onClick={() => (submit(e))} >Add To Cart</button>
@@ -60,7 +61,6 @@ function Laptops() {
                     </div>
                 </div>
             ))}
-
         </div>
     )
 }
--- tech/src/components/Mobiles.js
+++ tech/src/components/Mobiles.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Swal from 'sweetalert2'
 // import { useSelector, useDispatch } from "react-redux";
 // import { addToCart } from "../actions/index"
 
@@ -12,47 +13,45 @@ function Mobiles() {
             axios.get("http://localhost:3003/data")
                 .then((res) => {
                     setData(res.data);
-                    // console.log(res.data);
+                    console.log(res.data);
                 })
                 .catch((err) => {
-                      console.log(err);
+                    console.log(err);
 
                 }), []
     );
 
 
 
-  
+
 
 
     const submit = (product) => {
-        const updated = {quantity:1,...product}
-        console.log(updated)
-        axios.post('http://localhost:3003/addtocart',updated).then(res=>console.log(res)).catch(err=>console.log(err))
+        const updated = { quantity: 1, ...product }
+        // console.log(updated)
+        axios.post('http://localhost:3003/addtocart', updated).then(res => console.log(res)).catch(err => console.log(err))
 
-        // alert("product Added to cart")
-        setCartAnime(true);
-        setTimeout(()=>setCartAnime(false),2000);
+        Swal.fire(
+            `${updated.Dname}`,
+            'Added to cart',
+            'success'
+        )
 
     }
 
 
 
     return (
-        <div className='bg-gray-300'>
-            <div>
-
-            </div>
-
+        <div className='bg-gray-100 py-4'>
             {/* {data?.map((e, index) => ( */}
-                {data?.filter(fil=>fil.category == "Mobile").map((e, index) => (
-                    <div className=" flex   mx-32">
-                    {console.log(e)}
+            {data?.filter(fil => fil.category === "Mobile").map((e, index) => (
+                <div className=" flex bg-white rounded-xl shadow-lg mx-32 my-4">
+                    {/* {console.log(e)} */}
 
-                    <div className="border w-3/12 flex justify-center items-center bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className="w-3/12 flex justify-center items-center  m-4 p-4">
                         <img className="h-48" src={e.img} alt="" />
                     </div>
-                    <div className="border w-6/12 bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className="w-6/12  m-4 p-4">
                         <h2 className="text-3xl font-semibold">{e.Dname}</h2>
                         <div>
                             {e.spec.map((specs) => (
@@ -62,16 +61,15 @@ function Mobiles() {
                         <div className='text'>
                         </div>
                     </div>
-                    <div className=" flex flex-col justify-center items-center w-3/12 bg-white rounded-xl m-4 p-4 shadow-xl">
+                    <div className=" flex flex-col justify-center items-center w-3/12 m-4 p-4">
                         <h2 className="text-3xl font-semibold">{e.price}₹</h2>
                         <div className='flex justify-around items-center text-white text-lg'>
-                            <button className='bg-blue-400 p-2 rounded mx-1' onClick={() => (submit(e))} >{cartAnime? "Added" : "Add To Cart"}</button>
+                            <button className='bg-blue-400 p-2 rounded mx-1' onClick={() => (submit(e))} >Add To Cart</button>
                             {/* <button className='bg-yellow-300 p-2 rounded mx-1'>Buy Now</button> */}
                         </div>
                     </div>
                 </div>
             ))}
-
         </div>
     )
 }
